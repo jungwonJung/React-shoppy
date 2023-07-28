48e3b90e5fd8e88e646c99efad41c22554a3dbb6
jungwonJung <wjdwjd1501@gmail.com>
2023-07-28 20:53:38 +0200

    banner change and UI update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Banner() {
+  const navigate = useNavigate();
+  const handleToProducts = () => {
+    navigate("/products");
+  };
   return (
-    <section className="h-96 bg-yellow-900 relative">
+    <section style={{ height: "700px" }} className=" bg-yellow-900 relative">
       <div
         className="w-full h-full bg-cover bg-banner opacity-80
       "
       />
-      <div className="absolute w-full top-32 text-center text-gray-50 drop-shadow-2xl">
-        <h2 className="text-6xl">Shop With US</h2>
-        <p className="text-2xl">Best Products, High Quality</p>
+      <div className="absolute w-full top-80 -right-24 text-left text-gray-50 drop-shadow-2xl">
+        <h2 className="text-5xl mb-2">Sell Products with easy way</h2>
+        <h2 className="text-5xl mb-2">Buy Produts with easy way</h2>
+        <h2 className="text-5xl mb-12 text-brand">Shoppy</h2>
+        <button
+          className="text-black border border-black p-3 text-sm font-bold"
+          onClick={handleToProducts}
+        >
+          EXPLORE NOW
+        </button>
       </div>
     </section>
   );
